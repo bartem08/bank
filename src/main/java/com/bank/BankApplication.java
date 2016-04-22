f165eff3c8ade4f416d1ea054ba05cfd01006529
Artem Baranovskiy <artem2549@gmail.com>
2016-04-22 12:36:58 +0300

    resource server and authorization server in application class

--- src/main/java/com/bank/BankApplication.java
+++ src/main/java/com/bank/BankApplication.java
@@ -4,19 +4,20 @@ import com.bank.config.CustomUserDetails;
 import com.bank.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.*;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.*;
 
-@Configuration
-@EnableAutoConfiguration
-@EnableResourceServer
-public class BankApplication extends ResourceServerConfigurerAdapter {
+@SpringBootApplication
+public class BankApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankApplication.class, args);
@@ -33,14 +34,53 @@ public class BankApplication extends ResourceServerConfigurerAdapter {
 		return inn -> new CustomUserDetails(repository.findByInn(inn));
 	}
 
-	@Override
-	public void configure(HttpSecurity http) throws Exception {
-		http
-				.authorizeRequests()
-				.antMatchers("/bank/users/register")
-				.permitAll()
-				.anyRequest()
-				.authenticated();
+	@Configuration
+	@EnableAuthorizationServer
+	protected static class OAuth2ServerConfig extends AuthorizationServerConfigurerAdapter {
+
+		@Autowired
+		private AuthenticationManager authenticationManager;
+
+		@Override
+		public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+			endpoints
+					.authenticationManager(authenticationManager)
+					.approvalStoreDisabled();
+		}
+
+		@Override
+		public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+			security.checkTokenAccess("isAuthenticated()");
+		}
+
+		@Override
+		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+			clients.inMemory()
+					.withClient("android-client")
+					.authorizedGrantTypes("password", "implicit", "authorization_code")
+					.scopes("trust")
+					.resourceIds("oauth2-resource")
+					.accessTokenValiditySeconds(600)
+					.secret("android-client");
+		}
+
+	}
+
+	@Configuration
+	@EnableResourceServer
+	protected static class ResourceServer extends ResourceServerConfigurerAdapter {
+
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+			http
+					.csrf()
+					.disable()
+					.authorizeRequests()
+					.antMatchers("/bank/users/register")
+					.permitAll()
+					.anyRequest()
+					.authenticated();
+		}
 	}
 
 }
