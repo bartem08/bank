dc4f2ee5344ae6e9f2b916189f2f0024c23b287f
bartem08 <artem2549@gmail.com>
2016-05-07 23:06:41 +0300

    Update MailConfig.java
    deleted comments

--- src/main/java/com/bank/config/MailConfig.java
+++ src/main/java/com/bank/config/MailConfig.java
@@ -14,25 +14,11 @@ public class MailConfig {
     public JavaMailSender javaMailService() {
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
 
-        /*javaMailSender.setHost("smtp.gmail.com");
-        javaMailSender.setPort(587);
-        javaMailSender.setUsername("artem2549@gmail.com");
-        javaMailSender.setPassword("npb5d6qbr7lrl1");
-        javaMailSender.setJavaMailProperties(getMailProperties());*/
-
         javaMailSender.setHost("smtp.mailgun.org");
         javaMailSender.setPort(587);
         javaMailSender.setUsername("postmaster@appc6c1305178314ebe9fab84460643d30c.mailgun.org");
         javaMailSender.setPassword("4355942a1e2d21c137cf6bff8be67803");
-        //javaMailSender.setJavaMailProperties(getMailProperties());
 
         return javaMailSender;
-    }
-
-    private Properties getMailProperties() {
-        Properties properties = new Properties();
-        properties.setProperty("mail.smtp.auth", "true");
-        properties.setProperty("mail.smtp.starttls.enable", "true");
-        return properties;
     }
 }
