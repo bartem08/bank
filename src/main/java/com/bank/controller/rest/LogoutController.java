06524843bc0351ba593650e9b5f9f4f9293af980
Artem Baranovskiy <artem2549@gmail.com>
2016-05-23 21:15:05 +0300

    added logout controller

---
+++ src/main/java/com/bank/controller/rest/LogoutController.java
@@ -0,0 +1,31 @@
+package com.bank.controller.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class LogoutController {
+    @Autowired
+    @Qualifier("tokenStore")
+    private TokenStore tokenStore;
+
+    @RequestMapping(value = "/oauth/logout", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public void logout(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null) {
+            String tokenValue = authHeader.replace("Bearer", "").trim();
+            OAuth2AccessToken accessToken = tokenStore.readAccessToken(tokenValue);
+            tokenStore.removeAccessToken(accessToken);
+        }
+    }
+}
