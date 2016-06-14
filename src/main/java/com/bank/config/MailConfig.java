2718901dca5bdc6b0b6ac7f9aaa7e3202fba359b
bartem08 <artem2549@gmail.com>
2016-06-14 22:29:50 +0300

    hide
    hide

--- src/main/java/com/bank/config/MailConfig.java
+++ src/main/java/com/bank/config/MailConfig.java
@@ -16,8 +16,8 @@ public class MailConfig {
 
         javaMailSender.setHost("smtp.mailgun.org");
         javaMailSender.setPort(587);
-        javaMailSender.setUsername("postmaster@appc6c1305178314ebe9fab84460643d30c.mailgun.org");
-        javaMailSender.setPassword("4355942a1e2d21c137cf6bff8be67803");
+        javaMailSender.setUsername("*****");
+        javaMailSender.setPassword("****");
 
         return javaMailSender;
     }
